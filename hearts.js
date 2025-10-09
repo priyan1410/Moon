aa514bafaf3b6bdb33053491dea64310c4a74b3c
priyan1410 <142080876+priyan1410@users.noreply.github.com>
2025-10-09 12:01:10 +0530

    Update hearts.js

--- hearts.js
+++ hearts.js
@@ -407,7 +407,7 @@ function sendPasswordStatusEmail(status, attemptValue) {
   .catch(err => console.error("Error sending password attempt email:", err));
 }
 function checkMyPass() {
-  const correctPass = "----"; // your password
+  const correctPass = "1410"; // your password
   const inputEl = document.getElementById("myPassInput");
   const input = inputEl ? inputEl.value : "";
   const message = document.getElementById("myPassMessage");
